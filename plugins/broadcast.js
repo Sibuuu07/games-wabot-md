916589d3f35058b7c4bc2ce8d8f1c9265590a2ea
Sibu <95744973+Sibuuu07@users.noreply.github.com>
2022-05-03 12:03:47 +0530

    Update broadcast.js

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -4,11 +4,11 @@ let handler = async (m, { conn, text }) => {
   let chats = Object.entries(conn.chats).filter(([_, chat]) => chat.isChats).map(v => v[0])
   let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
   let teks = text ? text : cc.text
-  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
-  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 ' + author + ' All Chat Broadcast 」\n' + randomID(32)), true).catch(_ => _)
-  m.reply('Selesai Broadcast All Chat :)')
+  conn.reply(m.chat, `_Send a broadcast message to ${chats.length} chat_`, m)
+  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '❰ ' + author + ' 𝐀𝐑𝐂𝐓𝐈𝐗  𝐁𝐑𝐎𝐀𝐃𝐂𝐀𝐒𝐓 ❱\n' + randomID(32)), true).catch(_ => _)
+  m.reply('Done Broadcast All Chat :)')
 }
-handler.help = ['broadcast', 'bc'].map(v => v + ' <teks>')
+handler.help = ['broadcast', 'bc'].map(v => v + '')
 handler.tags = ['owner']
 handler.command = /^(broadcast|bc)$/i
 
