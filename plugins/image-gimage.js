f411b3faf6a163299947d8e9c3ef67f5347e35c0
Sibu <95744973+Sibuuu07@users.noreply.github.com>
2022-05-03 14:28:37 +0530

    Update image-gimage.js

--- plugins/image-gimage.js
+++ plugins/image-gimage.js
@@ -6,9 +6,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 *── 「 GOOGLE IMAGE 」 ──*
 
 Result from *${text}*
-`.trim(),[
-['Next', `${usedPrefix}gimage ${text}`]
-], m)
+`.trim() m)
 }
 handler.help = ['gimage <query>', 'image <query>']
 handler.tags = ['internet', 'tools']
