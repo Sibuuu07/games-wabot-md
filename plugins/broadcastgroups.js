80f8c7637af546190e36728c5aa0f93394a83860
Sibu <95744973+Sibuuu07@users.noreply.github.com>
2022-05-03 12:05:21 +0530

    Update broadcastgroups.js

--- plugins/broadcastgroups.js
+++ plugins/broadcastgroups.js
@@ -4,11 +4,11 @@ let handler = async (m, { conn, text }) => {
   let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
   let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
   let teks = text ? text : cc.text
-  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_`, m)
-  for (let id of groups) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 All Group Broadcast 」\n' + randomID(32)), true).catch(_ => _)
-  m.reply('Selesai Broadcast All Group :)')
+  conn.reply(m.chat, `_Send a broadcast message to ${groups.length} grup_`, m)
+  for (let id of groups) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 𝐀𝐫𝐜𝐭𝐢𝐱 𝐢𝐧𝐜. 𝐁𝐫𝐨𝐚𝐝𝐜𝐚𝐬𝐭 」\n' + randomID(32)), true).catch(_ => _)
+  m.reply('DONE Broadcast All Group :)')
 }
-handler.help = ['broadcastgroup', 'bcgc'].map(v => v + ' <teks>')
+handler.help = ['broadcastgroup', 'bcgc'].map(v => v + '')
 handler.tags = ['owner']
 handler.command = /^(broadcast|bc)(group|grup|gc)$/i
 
